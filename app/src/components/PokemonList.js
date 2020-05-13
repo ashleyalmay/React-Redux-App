133550bda3a669731bae951bc3d25f8e03cdbf2b
Ashley <54685244+ashleyalmay@users.noreply.github.com>
2020-05-13 18:55:09 -0400

    mvp but I want to expand and add there sprites and the dex #

---
+++ app/src/components/PokemonList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {getPokemon} from '../store/action';
+
+const PokemonList = props => {
+    const fetchPokemon = e => {
+        e.preventDefault();
+        props.getPokemon();
+    };
+    return(
+        <>
+        <h1> PokeDex</h1>
+        <div>
+            {props.pokemon.map(pokemon =>(
+                <h4 key={pokemon.url}>{pokemon.name}</h4>
+            ))}
+        </div>
+            {props.error && <p className="error">{props.error}</p>}
+            <button onClick={fetchPokemon}>Get Pokemon</button>
+            </>
+    );
+};
+
+const mapStateToProps = state => ({
+    pokemon: state.pokemon,
+    error: state.error
+});
+
+export default connect(
+    mapStateToProps,
+    {getPokemon}
+)(PokemonList);
